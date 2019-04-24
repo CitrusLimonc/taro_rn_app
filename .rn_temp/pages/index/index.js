973d83f6235cbce6a13c83992c675af3b9147255
itsonglei <190835837@qq.com>
2019-04-24 21:31:33 +0800

    init .rn_temp commit

---
+++ .rn_temp/pages/index/index.js
@@ -0,0 +1,32 @@
+var _class, _temp;
+
+import React from 'react';
+import { Component } from "@tarojs/taro-rn";
+import { View, Text } from "@tarojs/components-rn";
+import indexStyleSheet from "./index_styles";
+var _styleSheet = indexStyleSheet;
+let Index = (_temp = _class = class Index extends Component {
+  constructor() {
+    super(...arguments);
+    /**
+     * 指定config的类型声明为: Taro.Config
+     *
+     * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+     * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+     * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+     */
+  }
+  componentWillMount() {}
+  componentDidMount() {}
+  componentWillUnmount() {}
+  componentDidShow() {}
+  componentDidHide() {}
+  render() {
+    return <View style={_styleSheet["index"]}>
+        <Text>Hello world!</Text>
+      </View>;
+  }
+}, _class.config = {
+  navigationBarTitleText: '首页'
+}, _temp);
+export { Index as default };
